8a4a86d3c498b34fc493cc1f4c4bcd25a74d3b86
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-08 01:11:00 -0300

    criação do component post

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import styles from './App.module.css'
 import { Header } from "./components/header"
+import { Post } from './components/Post'
 import { SideBar } from './components/SideBar'
 import './global.css'
-import { Post } from './Post'
+
 export function App() {
 
 
@@ -12,18 +13,11 @@ export function App() {
       <div className={styles.wrapper}>
         <SideBar />
         <main>
-        <Post 
-            author="Rafael Dias" 
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, voluptatum alias. Eius dolorum doloribus, blanditiis commodi officia nostrum veritatis sed praesentium natus optio eaque? Molestias est corporis pariatur aliquam laudantium?"
-          />
-          <Post 
-            author="Josimeire" 
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum qui voluptates voluptatem. Aperiam delectus sequi saepe dolore reprehenderit aut nesciunt? Praesentium, nesciunt dicta amet eveniet iste consequatur impedit. Porro, odio?"
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </>
-    
     
   )
 }
